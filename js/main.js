6649b32a25bced400b93ccaf2d114db721a5db92
Agustin Barrero <agustinbarrero5k@gmail.com>
2022-10-18 17:14:27 -0300

    Hice las primeras cosas de javascript

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let criptoBtc = 19220.50;
+
+function conversorCripto(){
+//Obtengo elements html
+let conversorDolar = document.getElementById("dolarFilaUno");
+let conversorCripto = document.getElementById("criptoFilaUno");
+
+//Escribo valores de los elementos html y leo
+let  valueIngresado = document.getElementById("dolarFilaUno").value;
+let valueConvertido = valueIngresado / criptoBtc
+
+//Y aca deberia teoricamente settear los valores xd
+conversorCripto.setAttribute("value" , valueConvertido);
+
+
+
+    // console.log('El valor de calculodoDolarCripto es ' + conversorCripto)
+}
