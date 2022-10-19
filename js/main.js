c54dbc6e094ba4044776ae5c6b264f7c5ec5e1bd
Agustin Barrero <agustinbarrero5k@gmail.com>
2022-10-18 21:55:32 -0300

    Agregue el codigo javascript del conversor de etherum a bitcoin

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let criptoBtc = 19220.50;
 
-function conversorCripto(){
+function conversorCriptoBtc(){
 //Obtengo elements html
 let conversorDolar = document.getElementById("dolarFilaUno");
 let conversorCripto = document.getElementById("criptoFilaUno");
@@ -13,4 +13,17 @@ let valueConvertido = valueIngresado / criptoBtc
 conversorCripto.setAttribute("value" , valueConvertido);
 
     // console.log('El valor de calculodoDolarCripto es ' + conversorCripto)
+}
+let criptoEther = 14.75;
+function conversorCriptoEther(){
+    //Obtengo los elementos de html
+let conversorBtc = document.getElementById("btcFilaDos");
+let conversorEther = document.getElementById("etherFilaDos")
+
+//Escribo los valores de los elementos y los leo
+let valueIngresadoDos = document.getElementById("btcFilaDos").value;
+let valueConvertidoDos = valueIngresadoDos * criptoEther
+
+//Ahora setteo los valores
+conversorEther.setAttribute("value", valueConvertidoDos);
 }
