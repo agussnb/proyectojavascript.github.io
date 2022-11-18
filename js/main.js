f51d9631d6580937961b28dbbdd635643b5bbbf9
Agustin Barrero <agustinbarrero5k@gmail.com>
2022-11-18 17:24:03 -0300

    Hice lo mismo que hice a la mañana con el html de divisas y el js, pero en criptomonedas.html y su codigo js

--- js/main.js
+++ js/main.js
@@ -3,22 +3,22 @@
 //------------- Array con las monedas
 
 const moneda = [
-    {ejemplo:"5 Usd$", nombreMoneda:"Dolar", precioMoneda: 291, id:"dolar"},
-    {ejemplo:"5 Eu€", nombreMoneda:"Euro",precioMoneda:295, id:"euro"},
-    {ejemplo:"5 Jpy¥", nombreMoneda:"Yen",precioMoneda:1.17, id:"yen"},
-    {ejemplo:"5 Cny¥", nombreMoneda:"Yuan",precioMoneda:22.91, id:"yuan"},
-    {ejemplo:"5 Rub₽", nombreMoneda:"Rublo",precioMoneda:2.69, id:"rublo"},
-    {ejemplo:"5 Arg$", nombreMoneda:"PesoArg",precioMoneda:1, id:"pesoArg"},
-    {ejemplo:"5 Chl$", nombreMoneda:"PesoChl",precioMoneda:0.18, id:"pesoChl"},
-    {ejemplo:"5 Mex$", nombreMoneda:"PesoMex",precioMoneda:8.39, id:"pesoMex"}
+    {simbolo:"$", nombreMoneda:"Dolar", precioMoneda: 291, id:"dolar"},
+    {simbolo:"€", nombreMoneda:"Euro",precioMoneda:295, id:"euro"},
+    {simbolo:"¥", nombreMoneda:"Yen",precioMoneda:1.17, id:"yen"},
+    {simbolo:"¥", nombreMoneda:"Yuan",precioMoneda:22.91, id:"yuan"},
+    {simbolo:"₽", nombreMoneda:"Rublo",precioMoneda:2.69, id:"rublo"},
+    {simbolo:"$", nombreMoneda:"PesoArg",precioMoneda:1, id:"pesoArg"},
+    {simbolo:"$", nombreMoneda:"PesoChl",precioMoneda:0.18, id:"pesoChl"},
+    {simbolo:"$", nombreMoneda:"PesoMex",precioMoneda:8.39, id:"pesoMex"}
 ]
 //Selecciono el div vacio con getElementById y luego hago una funcion que muestre en el html lo que necesito
 const containerDivisa = document.getElementById('containerDivisa');
 function seleccionDivisa(moneda){
-    const {nombreMoneda, precioMoneda} = moneda
+    const {nombreMoneda, precioMoneda} = moneda;
 
     return `<div>
-                    <h3>${nombreMoneda}</h3>
+                    <h3 class="h3NombreMoneda">${nombreMoneda + moneda.simbolo}</h3>
                     <p>El precio es de: $${precioMoneda}</p>
                     <input type="text" id="monto">¿Cuanto deseas convertir?</input>
                     <button onclick="calcular(${precioMoneda})">Calcular</button>
@@ -43,4 +43,49 @@ function calcular(precioMoneda){
     console.log(result)
 
     resultContainer.innerHTML = `$${result}`
+}
+// Arranco con la parte de las criptos
+
+//----- Array con las criptos
+const cripto = [
+{nombreCripto:"Bitcoin",precioCripto:16582.8, id:"bitcoin"},
+{nombreCripto:"Ethereum",precioCripto:1204.25, id:"ethereum"},
+{nombreCripto:"Usdc",precioCripto:1, id:"usdc"},
+{nombreCripto:"Litecoin",precioCripto:0.01,id:"litecoin"},
+{nombreCripto:"Dai",precioCripto:1, id:"dai"},
+{nombreCripto:"Uniswap",precioCripto:5.79,id:"uniswap"},
+{nombreCripto:"Axs",precioCripto:7.03,id:"axs"},
+{nombreCripto:"Xrp",precioCripto:0.38,id:"xrp"}
+]
+//Selecciono el div vacio con getElementById y luego hago una funcion que muestre en el html lo que necesito
+const containerCripto = document.getElementById("containerCripto");
+function seleccionCripto(cripto){
+    const {nombreCripto, precioCripto} = cripto;
+    
+    return `<div>
+                         <h3 class="h3NombreCripto">${nombreCripto}</h3>
+                         <p>El precio es de dolares: $${precioCripto}</p>
+                         <input type="text" id="montoCripto">¿Cuanto deseas convertir?</input>
+                         <button onclick="calcular(${precioCripto})">Calcular</button>
+                         <p>El total es de dolares: </p>
+                         <p id="resultadoCripto"></p>
+                </div>
+                `
+}
+//Hago una funcion que me busque el find de la divisa requerida
+function renderCripto(id){
+    let resultCripto =cripto.find(cripto => cripto.id == id)
+    console.log(resultCripto)
+    containerCripto.innerHTML = seleccionCripto(resultCripto)
+}
+//Hago una funcion que sirve para hacer los calculos
+function calcular(precioCripto){
+    let montoIngresadoCripto = document.getElementById("montoCripto");
+    let resultContainerCripto = document.getElementById("resultadoCripto");
+
+    let resultCripto = montoIngresadoCripto.value * precioCripto;
+    console.log(montoIngresadoCripto.value);
+    console.log(resultCripto)
+
+    resultContainerCripto.innerHTML = `$${resultCripto}`
 }
